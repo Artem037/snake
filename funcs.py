fba8729c783bdc5f75c225981a7f384e3fef5cd7
Romasiktlt <118460134+Romasiktlt@users.noreply.github.com>
2023-01-19 16:40:52 +0400

    more funcs added

--- funcs.py
+++ funcs.py
@@ -81,3 +81,78 @@ def game_time_drawing(gameTime, score_font, score_numb_font, screen):
     game_time_numb = score_numb_font.render(str(gameTime / 1000), 1, pygame.Color("red"))
     screen.blit(game_time, (30, 10))
     screen.blit(game_time_numb, (105, 14))
+
+  
+def get_res():
+    # вывод результатов
+    with sqlite3.connect('snake_database.db') as db:
+        cursor = db.cursor()
+
+        query2 = '''SELECT * from players'''
+
+        data = cursor.execute(query2).fetchall()
+
+        db.commit()
+        cursor.close()
+    if data:
+        return data
+
+
+def terminate():
+    pygame.quit()
+    sys.exit()
+
+
+def start_screen(screen, clock, FPS, w, h):
+    intro_text = ["ЗМЕЙКА", "",
+                  "Нажмите любую кнопку,",
+                  "чтобы начать игру."]
+    fon = pygame.transform.scale(load_image('fon.jpeg'), (w, h))
+    screen.blit(fon, (0, 0))
+    font = pygame.font.Font(None, 30)
+    text_coord = 50
+    for line in intro_text:
+        string_rendered = font.render(line, 1, pygame.Color('white'))
+        intro_rect = string_rendered.get_rect()
+        text_coord += 10
+        intro_rect.top = text_coord
+        intro_rect.x = 10
+        text_coord += intro_rect.height
+        screen.blit(string_rendered, intro_rect)
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                terminate()
+            elif event.type == pygame.KEYDOWN or \
+                    event.type == pygame.MOUSEBUTTONDOWN:
+                return  # начинаем игру
+        pygame.display.flip()
+        clock.tick(FPS)
+
+
+def load_image(name, colorkey=None):
+    fullname = os.path.join('data', name)
+    if not os.path.isfile(fullname):
+        print(f"Файл с изображением '{fullname}' не найден")
+        sys.exit()
+    image = pygame.image.load(fullname)
+    if colorkey is not None:
+        image = image.convert()
+        if colorkey == -1:
+            colorkey = image.get_at((0, 0))
+        image.set_colorkey(colorkey)
+    else:
+        image = image.convert_alpha()
+    return image
+
+
+def resupdate(res):
+    # сохранение результатов
+    with sqlite3.connect('snake_database.db') as db:
+        cursor = db.cursor()
+
+        query = f"""INSERT INTO players(points) VALUES ({res})"""
+        cursor.execute(query)
+
+        db.commit()
+        cursor.close()
